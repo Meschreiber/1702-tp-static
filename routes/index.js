30c9b2af8eefdd0b385befc8d22360065287461b
Maria Schreiber <meschreiber3@gmail.com>
2017-03-17 17:31:51 -0400

    index renders all hotels, restaurants, activities

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var Promise = require('bluebird');
+var models = require('../models')
+var Place = models.Place;
+var Hotel = models.Hotel;
+var Restaurant = models.Restaurant;
+var Activity = models.Activity;
+var db = models.db;
 
 router.get('/', function(req, res, next){
-  res.render('index');
+  Promise.all([Hotel.findAll({}), Restaurant.findAll({}), Activity.findAll({})])
+  .spread(function(hotels, restaurants, activities ){    
+    res.render('index', {restaurants: restaurants, activities: activities, hotels: hotels});
+  })
+  .catch(next);
+
 });
 
 
