fdc72ec547c82730100a79ad81fb3ea2b854bcf9
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-03-17 13:06:50 -0400

    lunch time

--- app.js
+++ app.js
@@ -2,21 +2,30 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var nunjucks = require('nunjucks');
+const routes = require('./routes');
 
 var app = express();
+
+// nunjucks
+app.engine('html', nunjucks.render); // how to render html templates
+app.set('view engine', 'html'); // what file extension do our templates have
+nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 app.use(function(err, req, res, next) {
-	console.error(err)
-	res.status(500)
-	res.send(err, err.stack)
-})
+	console.error(err);
+	res.status(500);
+	res.send(err, err.stack);
+});
+
+app.use('/', routes);
 
 app.listen(3000, function(){
-	console.log("Listening on 3000")
-})
+	console.log('Listening on 3000');
+});
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
+var express = require('express');
+var router = express.Router();
 var Promise = require('bluebird');
+
+router.get('/', function(req, res, next){
+  res.render('index');
+});
+
+
+
+
+module.exports = router;
